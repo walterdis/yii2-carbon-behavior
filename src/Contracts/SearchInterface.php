7ad8fe1ebb0599d359b8197c80ccc296475975f5
= <=>
2019-05-05 21:59:38 -0300

    Initial alpha version.

---
+++ src/Contracts/SearchInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Walterdis\Yii2\Behavior\CarbonBehavior\Contracts;
+
+/**
+ * @author  Walter Discher Cechinel <mistrim@gmail.com>
+ *
+ * @package CarbonBehavior\Contracts
+ */
+interface SearchInterface
+{
+
+}
